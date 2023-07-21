8b10060247d8b80a44de3f4d6bae6a78867d850f
JDK <jd.kilgallin@gmail.com>
2023-07-21 09:32:04 -0400

    Site structure in place

--- site.py
+++ site.py
@@ -1,10 +1,20 @@
 import cherrypy, json, time
 import commands
 
+def getFile(filename,mode="r"):
+  f = open(filename,mode)
+  contents = f.read()
+  f.close()
+  return contents
+
 class Root(object):
     @cherrypy.expose
-    def index(self):
-        return "Hello"
+    def home(self):
+        return getFile('static/site.html')
+        
+    def site_js(self):
+        return getFile('static/site.js')
+        
     @cherrypy.expose
     def on(self):
         commands.on("Parlor NE")
